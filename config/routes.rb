e71e43b6d4e86e5f1e0a5b25c0c483c4b9e70139
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-04-16 15:10:16 -0300

    Setup packwerk (#8)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  draw(:hello_world)
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
