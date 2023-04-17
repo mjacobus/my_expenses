a5081ccc14f7512e8141fc69f403b91184c1d5cc
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-04-16 22:05:20 -0300

    Fix dev sessions

--- packs/user_sessions/config/routes/social_login.rb
+++ packs/user_sessions/config/routes/social_login.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env.development?
-  get '/dev/login', to: 'development#login'
+  get '/dev/login', to: 'user_sessions/dev_sessions#login'
 end
 
 get '/auth/:provider/callback', to: 'user_sessions/oauth_sessions#create'
