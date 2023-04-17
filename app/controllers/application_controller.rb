a09a3209ddfc45e41d8528840b1e700fd5dcccda
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-04-16 22:37:27 -0300

    Remove stuff from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,6 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_page404
 
-  rescue_from ActiveRecord::DeleteRestrictionError do |exception|
-    if respond_to?(:show)
-      flash.now[:error] = t('app.messages.cannot_delete_record')
-      next show
-    end
-
-    raise exception
-  end
-
   private
 
   def render_page404(_error)
