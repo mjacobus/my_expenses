41dccac64122ad69cf3d228c4e9f763a757aca28
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-04-16 21:04:35 -0300

    Add home

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   draw(:hello_world)
   draw(:social_login)
 
-  root "hello_world#index"
+  root "home#index"
 end
