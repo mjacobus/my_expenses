74cf83d8f864de39bcb2bd435a72029c39656520
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-05-01 16:19:13 -0300

    Basic frontend setup with parcel

---
+++ frontend/app.js
@@ -0,0 +1,19 @@
+// frontend/app.js
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const HelloWorld = () => {
+  return <React.Fragment>
+    <h1>Hello World from react</h1>
+  </React.Fragment>
+}
+
+import "./styles.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <HelloWorld />
+  </React.StrictMode>
+);
