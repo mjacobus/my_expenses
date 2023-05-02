de9294cb2ba78dbbbc07ed68165aba43c8730169
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-05-02 15:30:11 -0300

    Add navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import Alert from "@mui/material/Alert";
+import Navigation from "./components/MainNavigation.tsx";
 
 import "./app.css";
+
 const App = function () {
-  return <Alert severity="success">Hello World!</Alert>;
+  return (
+    <React.Fragment>
+      <Navigation />
+      <Alert severity="success">Hello World!</Alert>;
+    </React.Fragment>
+  );
 };
 
 export default App;
