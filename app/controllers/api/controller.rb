317a52a39317940fe66d31531d42d598ce21d2d7
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-05-02 13:36:31 -0300

    Add /api/session endpoint

--- app/controllers/api/controller.rb
+++ app/controllers/api/controller.rb
@@ -1,6 +1,6 @@
-# frozen_string_literal: true  
+# frozen_string_literal: true
 
-class Api::Controller < ActionController::Base
+class Api::Controller < ApplicationController
   include UserSessions::ControllerMethods
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_page404
@@ -8,6 +8,12 @@ class Api::Controller < ActionController::Base
   private
 
   def render_page404(_error)
-    render 'application/404', status: :not_found
+    head status: 404
+  end
+
+  def require_authorization
+    unless current_user
+      head status: 401
+    end
   end
 end
