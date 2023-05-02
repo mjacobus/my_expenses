5d186025c18c6d910d19dad62d6b374ffac21720
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-05-02 00:04:14 -0300

    bundle exec rails webpacker:install:typescript

--- app/javascript/packs/frontend.js
+++
@@ -1,2 +0,0 @@
-import "../../../frontend/src/app.js";
-console.log("frontend is logged again");
---
+++ app/javascript/packs/frontend.ts
@@ -0,0 +1,8 @@
+import "../../../frontend/src/app.js";
+
+function sum(y: number, x: number) : number {
+  return y + x;
+}
+
+console.log("Hello from typescript")
+console.log(sum(1, 2))
