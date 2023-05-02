a0215ea53454bbc06653259ff52fc29760c0f855
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-05-02 17:13:44 -0300

    Fetch user from api

--- frontend/src/components/UserNavigation.tsx
+++ frontend/src/components/UserNavigation.tsx
@@ -1,20 +1,35 @@
-import React from "react";
-import Navigation, { Page, UserData } from "./Navigation";
+import React, { useState, useEffect } from "react";
+import Navigation, { Page } from "./Navigation";
+import api from "../utils/api";
+import UserData from "../types/UserData";
+
+const pages: Page[] = [
+  {
+    text: "Minhas despesas",
+    href: "/despesas",
+  },
+];
 
 function UserNavigation() {
-  const userData: UserData = {
-    name: "Marcelo Jacobus",
-    email: "marcelojacobus@gmail.com",
-    avatar:
-      "https://lh3.googleusercontent.com/a/AGNmyxZveEOuHtU-N9UL75chANiTx6tPFTRXEQni_PzgVw=s96-c",
-  };
+  const [userData, setUserData] = useState<UserData | null>(null);
 
-  const pages: Page[] = [
-    {
-      text: "Minhas despesas",
-      href: "/despesas",
-    },
-  ];
+  async function fetchData() {
+    const data = await api.fetchUserSession();
+
+    if (!data) {
+      throw new Error("User is not logged in");
+    }
+
+    setUserData(data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!userData) {
+    return <React.Fragment />;
+  }
 
   return <Navigation userData={userData} pages={pages} />;
 }
