343797d923a876ac886bc7cd5fb90a0ba1e8d0da
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-05-02 00:23:45 -0300

    Change test files

--- app/javascript/packs/frontend.ts
+++ app/javascript/packs/frontend.ts
@@ -1,8 +1,2 @@
 import "../../../frontend/src/app";
-
-function sum(y: number, x: number): number {
-  return y + x;
-}
-
-console.log("Hello from typescript");
-console.log(sum(1, 2));
+console.log("Hello from app/javascript/packs/frontend.ts");
