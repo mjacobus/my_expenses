c6039016c5abedd0ff8d0dfad533cf7f99394854
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-05-02 12:37:52 -0300

    Add material example

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Alert from "@mui/material/Alert";
 
 import "./app.css";
 const App = function () {
-  return <h1>Hello World!</h1>;
+  return <Alert severity="success">Hello World!</Alert>;
 };
 
 export default App;
