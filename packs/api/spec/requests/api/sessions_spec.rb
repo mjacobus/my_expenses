c6e113e186b2d7c902760aa857699828ebde2e6c
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-05-04 12:14:52 -0300

    Refactor tests

--- packs/api/spec/requests/api/sessions_spec.rb
+++ packs/api/spec/requests/api/sessions_spec.rb
@@ -4,46 +4,32 @@ require 'rails_helper'
 
 RSpec.describe Api::SessionsController do
   describe 'GET /index' do
-    it 'returns suser information when user is logged in' do
+    before do
       sign_in(user)
-
-      get '/api/session'
-
-      expect(response).to be_successful
-      expect(json_response).to eq(
-        {
-          name: user.name,
-          email: user.email,
-          avatar: user.avatar
-        }
-      )
-    end
-
-    it 'responds with 401 when user is not logged in' do
-      get '/api/session'
-
-      expect(response).to have_http_status(:unauthorized)
+      stub_endpoint(Api::Sessions::ShowEndpoint)
     end
 
     it 'responds with the correct endpoint' do
-      sign_in(user)
-
-      stub_endpoint(Api::Sessions::ShowEndpoint)
-
       get '/api/session'
 
       expect(response).to be_successful
       expect(response.body).to eq('ok')
     end
 
-    it 'instanciates endpoint with the correct arguments' do
-      sign_in(user)
-
-      stub_endpoint(Api::Sessions::ShowEndpoint)
-
+    it 'instantiates endpoint with the correct arguments' do
       get '/api/session'
 
       expect(Api::Sessions::ShowEndpoint).to have_received(:new).with(actor: user)
+    end
+
+    context 'when user is not logged in' do
+      let(:user) { nil }
+
+      it 'responds with 401 when user is not logged in' do
+        get '/api/session'
+
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
   end
 end
