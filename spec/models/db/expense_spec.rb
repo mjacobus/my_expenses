bfb076e99aabbb6672672927725c06c13833016e
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-05-04 10:19:55 -0300

    Add validation to expenses model

--- spec/models/db/expense_spec.rb
+++ spec/models/db/expense_spec.rb
@@ -3,5 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe Db::Expense do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:expense) { Fabricate.build(:expense) }
+
+  it 'has a valid fabric' do
+    expect(expense).to be_valid
+  end
+
+  it 'validates presence of description' do
+    expense.description = nil
+
+    expect(expense).not_to be_valid
+  end
+
+  it 'validates presence of amount' do
+    expense.amount = nil
+
+    expect(expense).not_to be_valid
+  end
+
+  it 'validates presence of expensed_at' do
+    expense.expensed_at = nil
+
+    expect(expense).not_to be_valid
+  end
 end
