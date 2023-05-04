26e26893f9a4ffd2ea2b7cf5ad0731f512066963
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-05-04 12:03:40 -0300

    Unit test endpoint

--- spec/support/request_spec_helper.rb
+++ spec/support/request_spec_helper.rb
@@ -10,5 +10,14 @@ module RequestSpecHelper
   included do
     let(:user) { Fabricate(:user) }
     let(:json_response) { JSON.parse(response.body, symbolize_names: true) }
+    let(:mock_endpoint) { instance_double(Api::Endpoint) }
+    let(:mock_endpoint_response) { Api::JsonResponse.new('ok') }
+  end
+
+  def stub_endpoint(endpoint_class)
+    allow(endpoint_class).to receive(:new).and_return(mock_endpoint)
+    allow(mock_endpoint).to receive(:create_response)
+      .with(request: instance_of(ActionDispatch::Request))
+      .and_return(mock_endpoint_response)
   end
 end
