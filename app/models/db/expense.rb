3bffe36d15fb9e53276c48fd5a4add493b2aaefb
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-05-04 10:19:55 -0300

    Add validation to expenses model

--- app/models/db/expense.rb
+++ app/models/db/expense.rb
@@ -2,4 +2,8 @@
 
 class Db::Expense < ApplicationRecord
   belongs_to :user
+
+  validates :description, presence: true
+  validates :amount, presence: true
+  validates :expensed_at, presence: true
 end
