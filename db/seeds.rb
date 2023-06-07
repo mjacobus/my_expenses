33b8c17931079257fd3c6387ff1d3e9867b8ae9d
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-06-07 14:57:25 -0300

    Add expenses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+def log(message)
+  Rails.logger.debug message
+  yield
+  Rails.logger.debug 'Done'
+end
+
+user = Db::User.first || Fabricate(:user)
+
+log('Creating expenses') do
+  20.times do
+    Fabricate(:expense, user: user)
+  end
+end
