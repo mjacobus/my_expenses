e4f22ae7c06981ffc7d03739d9248e232f4bdf85
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-06-07 15:30:45 -0300

    Add basic expenses page

---
+++ frontend/src/pages/Expenses/IndexPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Alert from "@mui/material/Alert";
+
+export default function IndexPage() {
+  return (
+    <>
+      <Alert>
+        This is the Expenses Page
+      </Alert>
+    </>
+  );
+}
+
