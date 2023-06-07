e313f4c28cd33dc8f381bff703d2137fa6cd3c31
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-06-07 16:25:08 -0300

    Fetch expenses from API

--- frontend/src/pages/Expenses/IndexPage.tsx
+++ frontend/src/pages/Expenses/IndexPage.tsx
@@ -1,13 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Alert from "@mui/material/Alert";
+import api from "../../utils/api";
 
-export default function IndexPage() {
+function ExpenseItem({ item }) {
+  return (
+    <li>
+      <span> {item.description}</span>
+      <span> {item.amount}</span>
+      <span> {item.expensed_at}</span>
+    </li>
+  );
+}
+
+function Expenses({ expenses }) {
+  return (
+    <ul>
+      {expenses.map((expense) => (
+        <ExpenseItem item={expense} key={expense.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Page({ children }) {
   return (
     <>
-      <Alert>
-        This is the Expenses Page
-      </Alert>
+      <h1>Minhas despesas</h1>
+      {children}
     </>
   );
 }
 
+export default function IndexPage() {
+  const [expenses, setExpenses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await api.fetchExpenses();
+    setExpenses(response.data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <Page>
+        <Alert>Loading</Alert>
+      </Page>
+    );
+  }
+
+  return (
+    <Page>
+      <Expenses expenses={expenses} />
+    </Page>
+  );
+}
