7fd59c4f2f35f8655bec0e5f74b2f0a06112a3e4
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-06-07 16:25:08 -0300

    Fetch expenses from API

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -15,6 +15,22 @@ async function fetchUserSession(): Promise<UserData | null> {
   return null;
 }
 
+async function fetchExpenses(): Promise<Object | null> {
+  console.log("[API]: fetchExpenses()");
+  const response = await fetch("/api/expenses", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    return response.json();
+  }
+
+  return null;
+}
+
 export default {
   fetchUserSession,
+  fetchExpenses,
 };
