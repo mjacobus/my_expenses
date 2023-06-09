dc17784b54de97fc284c3e7187a9e1569b00e33b
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-06-09 10:21:20 -0300

    Improve Layout

--- frontend/src/pages/layouts/LoggedInLayout.tsx
+++ frontend/src/pages/layouts/LoggedInLayout.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import UserNavigation from "../../components/UserNavigation";
 import { Context as UserContext } from "../../contexts/userContext";
+import Container from "@mui/material/Container";
 
 export default function LoggedInLayout({ children }: { children: any }) {
   const context = useContext(UserContext);
@@ -12,7 +13,7 @@ export default function LoggedInLayout({ children }: { children: any }) {
   return (
     <>
       <UserNavigation userData={context.userData} />
-      {children}
+      <Container>{children}</Container>
     </>
   );
 }
