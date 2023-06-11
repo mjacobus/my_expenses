0ec43fb4fcda1514dc02bdbb81ea23e88496f4d2
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-06-11 14:25:26 -0300

    Remove console.log

--- frontend/src/utils/withLayout.tsx
+++ frontend/src/utils/withLayout.tsx
@@ -2,6 +2,5 @@ import React from "react";
 import LoggedInLayout from "../pages/layouts/LoggedInLayout";
 
 export default function withLayout(element: any) {
-  console.log(element);
   return <LoggedInLayout>{element}</LoggedInLayout>;
 }
