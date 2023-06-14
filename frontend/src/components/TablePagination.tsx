c10475fb583cbb7fe5f21529b99220f921727d19
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-06-14 11:23:08 -0300

    Implement pagination in react

---
+++ frontend/src/components/TablePagination.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import TablePagination from "@mui/material/TablePagination";
+
+interface TablePaginationProps {
+  count: number;
+  page: number;
+  perPage: number;
+  setPage: (page: number) => void;
+  setPerPage: (perPage: number) => void;
+}
+
+export default function CustomPagination({
+  count = 0,
+  page = 1,
+  perPage = 100,
+  setPerPage = () => {},
+  setPage = () => {},
+}: TablePaginationProps) {
+  function onPageChange(
+    _event: React.MouseEvent<HTMLButtonElement> | null,
+    page: number
+  ) {
+    setPage(page);
+  }
+
+  function onRowsPerPageChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setPerPage(parseInt(event.target.value));
+  }
+
+  return (
+    <TablePagination
+      component="div"
+      count={count}
+      page={page}
+      rowsPerPage={perPage}
+      onPageChange={onPageChange}
+      onRowsPerPageChange={onRowsPerPageChange}
+    />
+  );
+}
--- frontend/src/types/api.ts
+++ frontend/src/types/api.ts
@@ -1,5 +1,12 @@
 import Expense from "./Expense";
 
+interface Meta {
+  count: number;
+  page: number;
+  limit: number;
+}
+
 export interface ExpensesResponse {
   data: Expense[];
+  meta: Meta;
 }
