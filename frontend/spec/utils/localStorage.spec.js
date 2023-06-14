95a52adbf3d1db8e4970b261462eb03f2390e71d
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-06-14 13:44:33 -0300

    Add local storage helper

---
+++ frontend/spec/utils/localStorage.spec.js
@@ -0,0 +1,49 @@
+import { get, set, create, getInt } from "../../src/utils/localStorage";
+
+describe("LocalStorage", () => {
+  describe(".get", () => {
+    it("returns null if the key does not exist", () => {
+      expect(get("key")).toBe(null);
+    });
+
+    it("returns deafult value if the key does not exist", () => {
+      expect(get("key", "something-else")).toBe("something-else");
+    });
+
+    it("returns the value previsously set", () => {
+      set("name", "Marcelo");
+      const value = get("name");
+
+      expect(value).toEqual("Marcelo");
+    });
+  });
+
+  describe("create", () => {
+    it("segregates stores", () => {
+      const a = create("storage1");
+      const b = create("storage2");
+
+      a.set("someName", "Marcelo");
+
+      const values = [a.get("someName"), b.get("someName")];
+
+      expect(values).toEqual(["Marcelo", null]);
+    });
+  });
+
+  describe("getInt", () => {
+    it("returns null if the key does not exist", () => {
+      expect(getInt("someNumber")).toBe(null);
+    });
+
+    it("returns the default value when not set", () => {
+      expect(getInt("someNumber", 10)).toBe(10);
+    });
+
+    it("returns the defined value", () => {
+      set("theNumber", 10);
+
+      expect(getInt("theNumber")).toBe(10);
+    });
+  });
+});
---
+++ frontend/src/utils/localStorage.ts
@@ -0,0 +1,59 @@
+export default class LocalStorage {
+  name: String;
+  storage: Storage;
+
+  constructor(name: string = "__default", storage: Storage = localStorage) {
+    this.name = name;
+    this.storage = storage;
+  }
+
+  set(key: string, value: string) {
+    this.storage.setItem(`${this.name}:${key}`, value);
+  }
+
+  get(key: string, defaultValue: any = null): string | null {
+    const value = this.storage.getItem(`${this.name}:${key}`);
+
+    if (value === undefined) {
+      return null;
+    }
+
+    return value || defaultValue;
+  }
+
+  remove(key: string) {
+    this.storage.removeItem(`${this.name}:${key}`);
+  }
+
+  getInt(key: string, defaultValue: number | null = null): number | null {
+    const value = this.get(key);
+
+    if (value === null) {
+      return defaultValue;
+    }
+
+    return parseInt(value);
+  }
+}
+
+export function create(name: string): LocalStorage {
+  return new LocalStorage(name);
+}
+
+const defaultStorage = create("__default");
+
+export function get(item: string, defaultValue: any = null): string | null {
+  return defaultStorage.get(item, defaultValue);
+}
+
+export function set(item: string, value: string) {
+  return defaultStorage.set(item, value);
+}
+
+export function getInt(item: string, defaultValue: any): number | null {
+  return defaultStorage.getInt(item, defaultValue);
+}
+
+export function remove(item: string): void {
+  return defaultStorage.remove(item);
+}
