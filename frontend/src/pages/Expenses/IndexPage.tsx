9e9a108f357cf074cb4763ac2fb847ef94876868
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-06-14 11:23:08 -0300

    Implement pagination in react

--- frontend/src/pages/Expenses/IndexPage.tsx
+++ frontend/src/pages/Expenses/IndexPage.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Alert from "@mui/material/Alert";
 import api from "../../utils/api";
 import DataGrid from "../../components/DataGrid";
+import Pagination from "../../components/TablePagination";
 import Expense from "../../types/Expense";
 
 function Expenses({ expenses }: { expenses: Expense[] }) {
@@ -35,18 +36,25 @@ function Page({ children }: { children: React.ReactNode }) {
 export default function IndexPage() {
   const [expenses, setExpenses] = useState<Expense[]>([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(0); // it starts at 0
+  const [perPage, setPerPage] = useState(10);
+  const [count, setCount] = useState(1);
 
   const fetchData = async () => {
-    const response = await api.fetchExpenses();
+    const response = await api.fetchExpenses({
+      page: page + 1,
+      limit: perPage,
+    });
     if (response) {
       setExpenses(response.data);
+      setCount(response.meta.count);
     }
     setLoading(false);
   };
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [page, perPage]);
 
   if (loading) {
     return (
@@ -58,7 +66,21 @@ export default function IndexPage() {
 
   return (
     <Page>
+      <Pagination
+        setPerPage={setPerPage}
+        setPage={setPage}
+        count={count}
+        page={page}
+        perPage={perPage}
+      />
       <Expenses expenses={expenses} />
+      <Pagination
+        setPerPage={setPerPage}
+        setPage={setPage}
+        count={count}
+        page={page}
+        perPage={perPage}
+      />
     </Page>
   );
 }
--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,17 @@
 import UserData from "../types/UserData";
 import { ExpensesResponse } from "../types/api";
 
+function createUrl(path: string, params: any): URL {
+  const location = window.location;
+  const url = new URL(`${location.protocol}${location.host}/api/expenses`);
+
+  if (params) {
+    url.search = new URLSearchParams(params).toString();
+  }
+
+  return url;
+}
+
 async function fetchUserSession(): Promise<UserData | null> {
   console.log("[API]: fetchUserSession()");
   const response = await fetch("/api/session", {
@@ -16,9 +27,18 @@ async function fetchUserSession(): Promise<UserData | null> {
   return null;
 }
 
-async function fetchExpenses(): Promise<ExpensesResponse | null> {
-  console.log("[API]: fetchExpenses()");
-  const response = await fetch("/api/expenses", {
+interface FetchExpensesProps {
+  limit?: number;
+  page?: number;
+}
+
+async function fetchExpenses({
+  limit = 100,
+  page = 1,
+}: FetchExpensesProps): Promise<ExpensesResponse | null> {
+  console.log(`[API]: fetchExpenses(page = ${page}, limit = ${limit})`);
+  const url = createUrl("/api/expenses", { page, limit });
+  const response = await fetch(url, {
     headers: {
       "Content-Type": "application/json",
     },
