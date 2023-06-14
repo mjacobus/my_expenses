a38e1f177115cae5b87c5b4443fd53ebe9f26dde
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-06-14 17:06:53 -0300

    WIP

--- frontend/src/pages/Expenses/ListFilter.tsx
+++ frontend/src/pages/Expenses/ListFilter.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import Label from "@mui/material/InputLabel";
 
+// https://mui.com/x/react-data-grid/getting-started/
 export default function ListFilter() {
-  return <div>Filter</div>;
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Label>Expensed at:</Label>
+      <DatePicker label="From" />
+      <DatePicker label="To" />
+    </LocalizationProvider>
+  );
 }
